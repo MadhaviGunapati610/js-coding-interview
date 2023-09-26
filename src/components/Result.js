fcf78cc8d2ab032d032d720dbbed26db7493a804
Madhavi Gunapati <59458995+MadhaviGunapati610@users.noreply.github.com>
2023-09-26 20:37:48 +0000

    mortgage calculator

---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Result = () => {
+    return (
+        <>REsult</>
+    )
+}
+
+export default Result;
---
+++ src/components/TenureSelect.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TenureSelect = () => {
+    return (
+        <>Tenure Select</>
+    )
+}
+
+export default TenureSelect;
